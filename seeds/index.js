8d93d86d7ec650db4e17556a8aa88bfa791c1857
felipebertocchi <fabertocchi@gmail.com>
2023-03-19 18:25:33 -0300

    Add image, price & description to Campground Schema

--- seeds/index.js
+++ seeds/index.js
@@ -2,6 +2,7 @@ require('colors');
 const mongoose = require('mongoose');
 const Campground = require('../models/campground');
 const cities = require('./cities');
+const { getImage } = require('./getImage');
 const { places, descriptors } = require('./seedHelpers');
 
 mongoose.connect('mongodb://localhost:27017/yelp-camp', {
@@ -19,11 +20,16 @@ const sample = (arr) => arr[Math.floor(Math.random() * arr.length)];
 
 const seedDB = async () => {
     await Campground.deleteMany({});
-    for (let i = 0; i < 50; i++) {
+    console.log("generating new data...")
+    for (let i = 0; i < 2; i++) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = parseFloat(((Math.floor(Math.random() * 2000) + 10) / 100).toFixed(2));
         const randomCity = cities[random1000];
         const camp = new Campground({
             title: `${sample(descriptors)} ${sample(places)}`,
+            imageUrl: await getImage(),
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae tenetur minima, hic deleniti iste iusto, omnis pariatur doloribus quis necessitatibus tempora dicta aliquid recusandae esse? Quaerat tempora placeat autem doloribus.',
+            price: price,
             location: `${randomCity.city}, ${randomCity.state}`,
             latitude: `${randomCity.latitude}`,
             longitude: `${randomCity.longitude}`,
