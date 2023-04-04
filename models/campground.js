ada148fe6572107c83a4fc6b47bc8b3b14265c1d
felipebertocchi <fabertocchi@gmail.com>
2023-04-04 03:47:30 -0300

    Add campground deleteMany reviews middleware

--- models/campground.js
+++ models/campground.js
@@ -1,4 +1,5 @@
 const { Schema, model } = require('mongoose');
+const Review = require('./review');
 
 const CampgroundSchema = new Schema({
     title: String,
@@ -12,6 +13,16 @@ const CampgroundSchema = new Schema({
             ref: 'Review'
         }
     ]
+})
+
+CampgroundSchema.post(('findOneAndDelete'), async function (doc) {
+    if (doc) {
+        await Review.deleteMany({
+            _id: {
+                $in: doc.reviews
+            }
+        });
+    }
 })
 
 module.exports = model('Campground', CampgroundSchema);
