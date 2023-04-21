37425d2a28e912dfcf4e4893ebeb1c763bce5f7b
felipebertocchi <fabertocchi@gmail.com>
2023-04-21 03:30:30 -0300

    feat: store req.originalUrl in session as returnTo

--- middleware/authUser.js
+++ middleware/authUser.js
@@ -1,5 +1,6 @@
 module.exports = (req, res, next) => {
     if (!req.isAuthenticated()) {
+        req.session.returnTo = req.originalUrl;
         req.flash('error', 'You must sign in to continue');
         return res.redirect('/login');
     } else {
