b38d81a40dc0b3ba0b32e2f849e5083668b566c2
felipebertocchi <fabertocchi@gmail.com>
2023-04-24 01:33:22 -0300

    feat: add author to campground and review models

--- models/review.js
+++ models/review.js
@@ -3,6 +3,10 @@ const { Schema, model } = require('mongoose');
 const reviewSchema = new Schema({
     body: String,
     rating: Number,
+    author: {
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
     campground: {
         type: Schema.Types.ObjectId,
         ref: "Campground"
