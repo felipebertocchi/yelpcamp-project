704d6a7e7db9494cee1e81d8a94680bc0c75668c
felipebertocchi <fabertocchi@gmail.com>
2023-05-01 18:00:55 -0300

    refactor: import dotenv only on production env

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
+if (process.env.NODE_ENV !== "production") {
+    require('dotenv').config();
+}
+
 require('colors');
-require('dotenv').config();
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
