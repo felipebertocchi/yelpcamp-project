00e87851554ca3788ac7ce2dfff5a8c2ed64a9b9
felipebertocchi <fabertocchi@gmail.com>
2023-05-02 01:28:50 -0300

    fix(validateForms): remove tooltip code

--- public/javascript/validateForms.js
+++ public/javascript/validateForms.js
@@ -15,7 +15,4 @@
             form.classList.add('was-validated')
         }, false)
     })
-    
-    const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
-    const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
 })()
