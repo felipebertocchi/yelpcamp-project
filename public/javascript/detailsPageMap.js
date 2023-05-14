3b4b2684324ebab4d33fe3c5e1a4f156075873b1
felipebertocchi <fabertocchi@gmail.com>
2023-05-13 23:33:45 -0300

    feat(detailsPageMap): create map with marker

---
+++ public/javascript/detailsPageMap.js
@@ -0,0 +1,12 @@
+mapboxgl.accessToken = mapToken;
+
+const map = new mapboxgl.Map({
+    container: 'map',
+    style: 'mapbox://styles/mapbox/streets-v12',
+    center: campground.geometry.coordinates,
+    zoom: 9
+});
+
+new mapboxgl.Marker()
+    .setLngLat(campground.geometry.coordinates)
+    .addTo(map);
