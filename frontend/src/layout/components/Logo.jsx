c1994745eb910389101c3cde15a7e7ee0b35564b
felipebertocchi <fabertocchi@gmail.com>
2023-06-21 14:43:40 -0300

    feat(Logo): add Link to home page

--- frontend/src/layout/components/Logo.jsx
+++ frontend/src/layout/components/Logo.jsx
@@ -1,10 +1,11 @@
 import logo from '../../assets/logo.png';
 import logoDark from '../../assets/logo-dark.png';
 import { useMantineColorScheme } from '@mantine/core';
+import { Link } from 'react-router-dom';
 
 export default () => {
     const { colorScheme } = useMantineColorScheme();
     return (
-        <img src={colorScheme === 'dark' ? logoDark : logo} width={180} alt="logo" />
+        <Link to={'/'}><img src={colorScheme === 'dark' ? logoDark : logo} width={180} alt="logo" /></Link>
     );
 }
