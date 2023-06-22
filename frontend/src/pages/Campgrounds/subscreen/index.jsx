083402cd80fea05a37dfd3487967545d742cab93
felipebertocchi <fabertocchi@gmail.com>
2023-06-22 12:19:47 -0300

    feat(Campgrounds/subscreen): add gallery and ImageBox

--- frontend/src/pages/Campgrounds/subscreen/index.jsx
+++ frontend/src/pages/Campgrounds/subscreen/index.jsx
@@ -1,10 +1,13 @@
 import axios from "axios";
 import { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
+import { IconMapPin } from '@tabler/icons-react';
+import { Box, Grid, SimpleGrid, Title } from "@mantine/core";
+import ImageBox from "./components/ImageBox";
 
 export function Component() {
     const { campgroundId } = useParams();
-    const [campground, setCampground] = useState([]);
+    const [campground, setCampground] = useState(null);
 
     useEffect(() => {
         const getData = async () => {
@@ -13,7 +16,7 @@ export function Component() {
                     setCampground(response.data);
                 })
                 .catch(err => {
-                    console.log(err);
+                    console.error(err);
                 })
         }
         getData();
@@ -22,7 +25,29 @@ export function Component() {
     return (
         <>
             {campground &&
-                <h1>{campground.title}</h1>
+                <>
+                    <Title mb={10}>{campground.title}</Title>
+                    <div style={{ display: 'flex' }}>
+                        <IconMapPin />
+                        <Title order={3} ml={5}>{campground.location}</Title>
+                    </div>
+                    <Box my="md">
+                        <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'xs', cols: 1 }]}>
+                            <ImageBox src={campground.images[0]?.url} alt={campground.title} height={515} />
+                            <Grid gutter="md">
+                                <Grid.Col>
+                                    <ImageBox src={campground.images[1]?.url} alt={campground.title} height={250} />
+                                </Grid.Col>
+                                <Grid.Col span={6}>
+                                    <ImageBox src={campground.images[2]?.url} alt={campground.title} height={250} />
+                                </Grid.Col>
+                                <Grid.Col span={6}>
+                                    <ImageBox src={campground.images[3]?.url} alt={campground.title} height={250} />
+                                </Grid.Col>
+                            </Grid>
+                        </SimpleGrid>
+                    </Box>
+                </>
             }
         </>
     )
