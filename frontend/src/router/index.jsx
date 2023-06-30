6e846691f05898feb25da80ae47c592f4efe7444
felipebertocchi <fabertocchi@gmail.com>
2023-06-30 17:52:00 -0300

    feat: add ErrorPage

--- frontend/src/router/index.jsx
+++ frontend/src/router/index.jsx
@@ -1,12 +1,14 @@
 import { createBrowserRouter } from 'react-router-dom';
 import Layout from '../layout';
 import pages from '../pages';
+import { ErrorPage } from '../pages/ErrorPage';
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Layout />,
         children: pages,
+        errorElement: <ErrorPage />
     },
 ]);
 
