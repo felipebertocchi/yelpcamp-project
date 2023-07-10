0331a6e7ac2f183f0559174e3b7a034602e3fb0a
felipebertocchi <fabertocchi@gmail.com>
2023-07-10 11:39:38 -0300

    feat(reviewsController): edit responses

--- backend/controllers/reviewsController.js
+++ backend/controllers/reviewsController.js
@@ -3,21 +3,27 @@ const Review = require('../models/review');
 
 module.exports = {
     postNewReview: async (req, res) => {
-        const { id } = req.params;
-        const campground = await Campground.findById(id);
-        const review = new Review({ ...req.body.review, campground });
-        review.author = req.user._id;
-        campground.reviews.push(review)
-        await review.save();
-        await campground.save();
-        req.flash('success', 'Succesfully added a new review');
-        res.redirect(`/campgrounds/${campground._id}`);
+        try {
+            const { id } = req.params;
+            const campground = await Campground.findById(id);
+            const review = new Review({ ...req.body.review, campground });
+            review.author = req.user._id;
+            campground.reviews.push(review);
+            await review.save();
+            await campground.save();
+            res.status(200).json({ message: 'Successfully added a new review' });
+        } catch (error) {
+            res.status(500).json({ message: 'There was an error processing your review' });
+        }
     },
     deleteReview: async (req, res) => {
-        const { id, reviewId } = req.params;
-        await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
-        await Review.findByIdAndDelete(reviewId);
-        req.flash('success', 'Succesfully deleted review');
-        res.redirect(`/campgrounds/${id}`);
+        try {
+            const { id, reviewId } = req.params;
+            await Campground.findByIdAndUpdate(id, { $pull: { reviews: reviewId } });
+            await Review.findByIdAndDelete(reviewId);
+            res.status(200).json({ message: 'Successfully deleted your review' });
+        } catch (error) {
+            res.status(500).json({ message: 'There was an error deleting your review' });
+        }
     }
 }
