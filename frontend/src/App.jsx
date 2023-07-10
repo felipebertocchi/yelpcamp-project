5fa6d700ca68fa572edf2d57f0163212799e4e8f
felipebertocchi <fabertocchi@gmail.com>
2023-07-10 11:19:54 -0300

    feat(App): add Notifications component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,7 +1,8 @@
 import { RouterProvider } from 'react-router-dom';
 import { ColorSchemeProvider, MantineProvider } from "@mantine/core";
-import { AuthProvider } from './auth/AuthContext';
 import { useLocalStorage } from '@mantine/hooks';
+import { Notifications } from '@mantine/notifications';
+import { AuthProvider } from './auth/AuthContext';
 import router from './router';
 import axios from 'axios';
 
@@ -19,6 +20,7 @@ export default () => {
         <AuthProvider>
             <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
                 <MantineProvider withGlobalStyles withNormalizeCSS theme={{ colorScheme }} >
+                    <Notifications autoClose={8000} />
                     <RouterProvider router={router} />
                 </MantineProvider>
             </ColorSchemeProvider>
