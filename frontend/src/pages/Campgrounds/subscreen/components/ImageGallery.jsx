db3afb804921b74bb766327c746ff8011a205ad1
felipebertocchi <fabertocchi@gmail.com>
2023-07-12 00:26:31 -0300

    feat(ImageGallery): add multiple grid formats

--- frontend/src/pages/Campgrounds/subscreen/components/ImageGallery.jsx
+++ frontend/src/pages/Campgrounds/subscreen/components/ImageGallery.jsx
@@ -12,24 +12,87 @@ export default function ({ images }) {
         setInitialSlide(index);
     }
 
-    return (
-        <Box my="md">
-            <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'xs', cols: 1 }]}>
+    const PhotoGrid = () => {
+        if (images.length < 2) {
+            return (
                 <Box onClick={() => handleCarousel(0)}>
                     <ImageBox src={images[0]?.url} alt={"campground-image"} height={515} />
                 </Box>
-                <Grid gutter="md">
-                    <Grid.Col onClick={() => handleCarousel(1)}>
-                        <ImageBox src={images[1]?.url} alt={"campground-image"} height={250} />
-                    </Grid.Col>
-                    <Grid.Col onClick={() => handleCarousel(2)} span={6}>
-                        <ImageBox src={images[2]?.url} alt={"campground-image"} height={250} />
-                    </Grid.Col>
-                    <Grid.Col onClick={() => handleCarousel(3)} span={6}>
-                        <ImageBox src={images[3]?.url} alt={"campground-image"} height={250} />
-                    </Grid.Col>
-                </Grid>
-            </SimpleGrid>
+            )
+        } else if (images.length === 2) {
+            return (
+                <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'xs', cols: 1 }]}>
+                    <Box onClick={() => handleCarousel(0)}>
+                        <ImageBox src={images[0]?.url} alt={"campground-image"} height={515} />
+                    </Box>
+                    <Box onClick={() => handleCarousel(1)}>
+                        <ImageBox src={images[1]?.url} alt={"campground-image"} height={515} />
+                    </Box>
+                </SimpleGrid>
+            )
+        } else if (images.length === 3) {
+            return (
+                <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'xs', cols: 1 }]}>
+                    <Box onClick={() => handleCarousel(0)}>
+                        <ImageBox src={images[0]?.url} alt={"campground-image"} height={515} />
+                    </Box>
+                    <Grid gutter="md">
+                        <Grid.Col onClick={() => handleCarousel(1)}>
+                            <ImageBox src={images[1]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                        <Grid.Col onClick={() => handleCarousel(2)}>
+                            <ImageBox src={images[2]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                    </Grid>
+                </SimpleGrid>
+            )
+        } else if (images.length === 4) {
+            return (
+                <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'xs', cols: 1 }]}>
+                    <Box onClick={() => handleCarousel(0)}>
+                        <ImageBox src={images[0]?.url} alt={"campground-image"} height={515} />
+                    </Box>
+                    <Grid gutter="md">
+                        <Grid.Col onClick={() => handleCarousel(1)}>
+                            <ImageBox src={images[1]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                        <Grid.Col onClick={() => handleCarousel(2)} span={6}>
+                            <ImageBox src={images[2]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                        <Grid.Col onClick={() => handleCarousel(3)} span={6}>
+                            <ImageBox src={images[3]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                    </Grid>
+                </SimpleGrid>
+            )
+        } else if (images.length >= 5) {
+            return (
+                <SimpleGrid cols={2} spacing="md" breakpoints={[{ maxWidth: 'xs', cols: 1 }]}>
+                    <Box onClick={() => handleCarousel(0)}>
+                        <ImageBox src={images[0]?.url} alt={"campground-image"} height={515} />
+                    </Box>
+                    <Grid gutter="md">
+                        <Grid.Col onClick={() => handleCarousel(1)} span={6}>
+                            <ImageBox src={images[1]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                        <Grid.Col onClick={() => handleCarousel(2)} span={6}>
+                            <ImageBox src={images[2]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                        <Grid.Col onClick={() => handleCarousel(3)} span={6}>
+                            <ImageBox src={images[3]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                        <Grid.Col onClick={() => handleCarousel(4)} span={6}>
+                            <ImageBox src={images[4]?.url} alt={"campground-image"} height={250} />
+                        </Grid.Col>
+                    </Grid>
+                </SimpleGrid>
+            )
+        }
+    }
+
+    return (
+        <Box my="md">
+            <PhotoGrid />
             <FullScreenCarousel images={images} open={openCarousel} setOpen={setOpenCarousel} initialSlide={initialSlide} />
         </Box>
     )
