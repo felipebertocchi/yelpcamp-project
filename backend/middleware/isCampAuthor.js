11bf14d012906169d9f013c901ef7b066bb5e18b
felipebertocchi <fabertocchi@gmail.com>
2023-07-27 21:37:51 -0300

    feat(isCampAuthor): return response

--- backend/middleware/isCampAuthor.js
+++ backend/middleware/isCampAuthor.js
@@ -4,8 +4,7 @@ module.exports = async (req, res, next) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     if (!campground.author?.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that');
-        return res.redirect(`/campgrounds/${campground._id}`); 
+        return res.status(401).json({ message: "You do not have permission to do that", error });
     } else {
         next();
     }
