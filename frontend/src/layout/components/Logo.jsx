f159f84fd26255e61fc82582425b4512d4c9b6c1
felipebertocchi <fabertocchi@gmail.com>
2023-07-28 01:19:56 -0300

    feat(Logo): remove Link, add custom width prop

--- frontend/src/layout/components/Logo.jsx
+++ frontend/src/layout/components/Logo.jsx
@@ -1,11 +1,10 @@
 import logo from '../../assets/logo.png';
 import logoDark from '../../assets/logo-dark.png';
 import { useMantineColorScheme } from '@mantine/core';
-import { Link } from 'react-router-dom';
 
-export default () => {
+export default ({ width }) => {
     const { colorScheme } = useMantineColorScheme();
     return (
-        <Link to={'/'}><img src={colorScheme === 'dark' ? logoDark : logo} width={180} alt="logo" /></Link>
+        <img src={colorScheme === 'dark' ? logoDark : logo} width={width || 180} alt="logo" />
     );
 }
