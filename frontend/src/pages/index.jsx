442e7b62dd12bb66b39ba94619773676cfeb36a6
felipebertocchi <fabertocchi@gmail.com>
2023-07-28 01:38:27 -0300

    feat(pages): add Landing page

--- frontend/src/pages/index.jsx
+++ frontend/src/pages/index.jsx
@@ -1,8 +1,13 @@
 import { default as campgroundsConfig } from './Campgrounds/config'
 import { default as registerConfig } from './Register/config'
 import { default as loginConfig } from './Login/config'
+// import Home from './Landing'
 
 export default [
+    {
+        index: true,
+        lazy: () => import('./Landing'),
+    },
     campgroundsConfig,
     loginConfig,
     registerConfig,
