e3ba0578365b33116ff1183587a9fc7a0d8e16e9
felipebertocchi <fabertocchi@gmail.com>
2023-08-01 00:06:46 -0300

    refactor(Login): code formatting

--- frontend/src/pages/Login/index.jsx
+++ frontend/src/pages/Login/index.jsx
@@ -2,14 +2,13 @@ import { Container, Group, Paper } from "@mantine/core";
 import LoginForm from "./components/LoginForm";
 
 export function Component() {
-
     return (
-            <Container size={'xl'}>
-                <Group position="right">
-                    <Paper withBorder shadow="md" p={30} mt={30} radius="md" miw={400}>
-                        <LoginForm />
-                    </Paper>
-                </Group>
-            </Container>
+        <Container size={'xl'}>
+            <Group position="right">
+                <Paper withBorder shadow="md" p={30} mt={30} radius="md" miw={400}>
+                    <LoginForm />
+                </Paper>
+            </Group>
+        </Container>
     )
 }
