f063996c973020ee25d71c03f21e9179357ac335
felipebertocchi <fabertocchi@gmail.com>
2023-08-01 23:23:20 -0300

    fix(middleware): edit isReviewAuthor response

--- backend/middleware/isReviewAuthor.js
+++ backend/middleware/isReviewAuthor.js
@@ -1,11 +1,10 @@
 const Review = require("../models/review");
 
 module.exports = async (req, res, next) => {
-    const { id, reviewId } = req.params;
+    const { reviewId } = req.params;
     const review = await Review.findById(reviewId);
     if (!review.author?.equals(req.user._id)) {
-        req.flash('error', 'You do not have permission to do that');
-        return res.redirect(`/campgrounds/${id}`); 
+        return res.status(401).json('You do not have permission to do that'); 
     } else {
         next();
     }
