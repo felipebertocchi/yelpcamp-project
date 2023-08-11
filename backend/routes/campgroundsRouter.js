9d36dff3f75fef88c91b54a6615c7aad5e335043
felipebertocchi <fabertocchi@gmail.com>
2023-08-10 22:57:06 -0300

    refactor(routers): replace authUser middleware w/ verifyJwt

--- backend/routes/campgroundsRouter.js
+++ backend/routes/campgroundsRouter.js
@@ -1,21 +1,22 @@
 const express = require('express');
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const { authUser, isCampAuthor, validateCampground } = require('../middleware');
+const { isCampAuthor, validateCampground } = require('../middleware');
 const campgroundsController = require('../controllers/campgroundsController');
 const multer = require('multer');
 const { storage } = require('../cloudinary');
+const { verifyJwt } = require('../auth');
 const upload = multer({ storage });
 
 router.route('/')
     .get(catchAsync(campgroundsController.index))
-    .post(authUser, upload.array('images'), validateCampground, catchAsync(campgroundsController.postNewCampground));
+    .post(verifyJwt, upload.array('images'), validateCampground, catchAsync(campgroundsController.postNewCampground));
     // TODO Refactor middleware to use validate before multer
 
 router.route('/:id')
     .get(catchAsync(campgroundsController.getCampground))
-    .put(authUser, isCampAuthor, upload.array('images'), validateCampground, catchAsync(campgroundsController.updateCampground))
+    .put(verifyJwt, isCampAuthor, upload.array('images'), validateCampground, catchAsync(campgroundsController.updateCampground))
     // TODO Refactor middleware to use validate before multer
-    .delete(authUser, isCampAuthor, catchAsync(campgroundsController.deleteCampground));
+    .delete(verifyJwt, isCampAuthor, catchAsync(campgroundsController.deleteCampground));
 
 module.exports = router;
--- backend/routes/reviewsRouter.js
+++ backend/routes/reviewsRouter.js
@@ -1,10 +1,11 @@
 const express = require('express');
 const router = express.Router({ mergeParams: true });
 const catchAsync = require('../utils/catchAsync');
-const { authUser, isReviewAuthor, validateReview } = require('../middleware');
+const { isReviewAuthor, validateReview } = require('../middleware');
 const reviewsController = require('../controllers/reviewsController');
+const { verifyJwt } = require('../auth');
 
-router.post('/', authUser, validateReview, catchAsync(reviewsController.postNewReview));
-router.delete('/:reviewId', authUser, isReviewAuthor, catchAsync(reviewsController.deleteReview));
+router.post('/', verifyJwt, validateReview, catchAsync(reviewsController.postNewReview));
+router.delete('/:reviewId', verifyJwt, isReviewAuthor, catchAsync(reviewsController.deleteReview));
 
 module.exports = router;
