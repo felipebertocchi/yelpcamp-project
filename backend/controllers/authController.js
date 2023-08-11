cebb3b86483579942a051de50422b11fb0949e34
felipebertocchi <fabertocchi@gmail.com>
2023-08-10 22:56:17 -0300

    refactor(authController): return JWT in register and login

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,34 +1,44 @@
+const passport = require('passport');
 const User = require('../models/user');
+const { getToken } = require('../auth');
 
 module.exports = {
     registerNewUser: async (req, res) => {
+        const { name, email, phone, password } = req.body;
         try {
-            const { name, email, phone, password } = req.body;
-            const user = new User({ name, email, phone });
-            const registedUser = await User.register(user, password);
-            req.login(registedUser, function (err) {
-                if (err) return res.status(500).json({ message: 'User register failed', error: err });
-                return res.status(200).json({
-                    message: 'User registered successfully',
-                    verified: true,
-                    user: req.user,
-                });
-            });
+            User.register(
+                new User({ name, email, phone }),
+                password,
+                function (err, user, info) {
+                    if (err) {
+                        res.setHeader('Content-Type', 'application/json');
+                        res.status(500).json({ message: 'There was an error with the user registration', err, info });
+                    }
+                    else {
+                        passport.authenticate('local')(req, res, () => {
+                            const token = getToken({ userID: user._id, name: user.name });
+                            res.setHeader('Content-Type', 'application/json');
+                            res.status(200).json({ verified: true, status: 'Registration Successful', token });
+                        });
+                    }
+                }
+            );
         } catch (err) {
             console.log(err);
             return res.status(500).json({ message: 'Error registering user', error: err.message });
         }
     },
     loginUser: async (req, res) => {
-        if (req.isAuthenticated()) {
-            return res.status(200).json({
-                message: 'Login successful',
-                verified: true,
-                user: req.user
-            });
-        } else {
-            return res.status(500).json({ message: 'There was an error processing the login request' });
-        }
+        passport.authenticate("local", function (err, user, info) {
+            if (err) {
+                return res.status(500).json({ message: 'There was an error processing the login request', err });
+            }
+            if (!user) {
+                return res.status(500).json({ message: 'email or password is wrong', info });
+            }
+            const token = getToken({ userID: user._id, name: user.name });
+            return res.status(200).json({ verified: true, message: "Login successful", token });
+        })(req, res)
     },
     logoutUser: (req, res) => {
         req.logout(function (err) {
--- backend/routes/userRouter.js
+++ backend/routes/userRouter.js
@@ -1,31 +1,11 @@
 const express = require('express')
 const router = express.Router();
 const catchAsync = require('../utils/catchAsync');
-const passport = require('passport');
 const authController = require('../controllers/authController');
 
 router.post('/register', catchAsync(authController.registerNewUser));
 
-router.post('/login',
-        function (req, res, next) {
-            passport.authenticate('local', function (err, user, info) {
-                if (err) {
-                    console.error(err);
-                    return res.status(500).json({ message: 'Login failed', error: err });
-                }
-                if (!user) {
-                    return res.status(401).json({ error: info.message });
-                }
-                req.logIn(user, function (err) {
-                    if (err) {
-                        return next(err);
-                    }
-                    next();
-                });
-            })(req, res, next);
-        },
-        catchAsync(authController.loginUser)
-    );
+router.post('/login', catchAsync(authController.loginUser));
 
 router.get('/logout', authController.logoutUser);
 
