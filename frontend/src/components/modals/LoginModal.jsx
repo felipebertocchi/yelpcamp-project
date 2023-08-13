efc93494a181baf5fc9646485dbb844899fe7dc7
felipebertocchi <fabertocchi@gmail.com>
2023-08-13 19:45:10 -0300

    refactor(LoginModal): move children elements inside Paper

--- frontend/src/components/modals/LoginModal.jsx
+++ frontend/src/components/modals/LoginModal.jsx
@@ -5,8 +5,8 @@ import LoginForm from "../LoginForm";
 export default function ({ opened, close, preventRedirect, children }) {
     return (
         <CustomModal opened={opened} close={close}>
-            {children}
             <Paper p={30} radius="md" miw={400}>
+                {children}
                 <LoginForm preventRedirect={preventRedirect} closeModal={close} />
             </Paper>
         </CustomModal>
