251f1b7012ba9c33df675fbf959a41144613b12e
felipebertocchi <fabertocchi@gmail.com>
2023-08-13 20:36:18 -0300

    fix(LoginModal): execute onSuccess if defined

--- frontend/src/components/modals/LoginModal.jsx
+++ frontend/src/components/modals/LoginModal.jsx
@@ -10,7 +10,7 @@ export default function ({ opened, close, onSuccess, preventRedirect, children }
                 <LoginForm
                     preventRedirect={preventRedirect}
                     onSuccess={() => {
-                        onSuccess();
+                        if (onSuccess) onSuccess();
                         close();
                     }}
                 />
