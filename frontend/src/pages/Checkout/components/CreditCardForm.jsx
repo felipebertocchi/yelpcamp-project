7a139ed5388b978666043e39ec7a6665b1a75ebc
felipebertocchi <fabertocchi@gmail.com>
2023-09-14 20:56:13 -0300

    feat(Checkout): add Credit card form, fixes

---
+++ frontend/src/pages/Checkout/components/CreditCardForm.jsx
@@ -0,0 +1,74 @@
+import { Group, Input, Stack } from '@mantine/core';
+import React, { useState } from 'react';
+import Cards from 'react-credit-cards';
+import 'react-credit-cards/es/styles-compiled.css';
+
+const CreditCardForm = () => {
+    const [state, setState] = useState({
+        number: '',
+        expiry: '',
+        cvc: '',
+        name: '',
+        focus: '',
+    });
+
+    const handleInputChange = (evt) => {
+        const { name, value } = evt.target;
+
+        setState((prev) => ({ ...prev, [name]: value }));
+    }
+
+    const handleInputFocus = (evt) => {
+        setState((prev) => ({ ...prev, focus: evt.target.name }));
+    }
+
+    return (
+        <Group noWrap p="md">
+            <Cards
+                number={state.number}
+                expiry={state.expiry}
+                cvc={state.cvc}
+                name={state.name}
+                focused={state.focus}
+            />
+            <Stack px={10}>
+                <Input
+                    type="number"
+                    name="number"
+                    placeholder="Card Number"
+                    value={state.number}
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                />
+                <Input
+                    type="text"
+                    name="name"
+                    placeholder="Name"
+                    value={state.name}
+                    onChange={handleInputChange}
+                    onFocus={handleInputFocus}
+                />
+                <Group noWrap>
+                    <Input
+                        type="number"
+                        name="expiry"
+                        placeholder="Valid Thru"
+                        value={state.expiry}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                    />
+                    <Input
+                        type="number"
+                        name="cvc"
+                        placeholder="CVC"
+                        value={state.cvc}
+                        onChange={handleInputChange}
+                        onFocus={handleInputFocus}
+                    />
+                </Group>
+            </Stack>
+        </Group>
+    );
+}
+
+export default CreditCardForm;
--- frontend/src/pages/Checkout/index.jsx
+++ frontend/src/pages/Checkout/index.jsx
@@ -3,6 +3,7 @@ import { IconInfoCircle, IconStarFilled } from "@tabler/icons-react";
 import useCamp from "../../hooks/useCamp";
 import { useNavigate } from "react-router-dom";
 import { useEffect, useState } from "react";
+import CreditCardForm from "./components/CreditCardForm";
 
 const useStyles = createStyles((theme) => ({
     root: {
@@ -53,136 +54,132 @@ export function Component() {
 
     return (
         <Container size={"xl"} mt={60}>
-            <Grid gutter={55}>
-                <Grid.Col span={7}>
-                    <Title mb="lg">
-                        Book your stay at {campground.title}
-                    </Title>
-                    <Title order={3}>
-                        Dates
-                    </Title>
-                    <Text>
-                        {checkInDate.format("MMM DD")} - {checkOutDate.format("MMM DD")}
-                    </Text>
-                    <Divider my="lg" />
-                    <Title order={3} mb="lg">
-                        Choose how to pay
-                    </Title>
-                    <Paper withBorder>
-                        <Accordion
-                            mx="auto"
-                            variant="filled"
-                            defaultValue="cash"
-                            classNames={classes}
-                            className={classes.root}
-                        >
-
-                            <Accordion.Item value="cash">
-                                <Accordion.Control>
-                                    <Title order={4}>Cash</Title>
-                                </Accordion.Control>
-                                <Accordion.Panel>
-                                    <Paper p="lg" withBorder mb="xs" onClick={() => handleSelectPayment("cash-full")}>
-                                        <Group>
-                                            <Radio checked={payment === "cash-full"} defaultChecked/>
-                                            <div>
-                                                <Title order={4}>Pay in full</Title>
-                                                <Text>Pay the total (${checkoutDetails.totalBeforeTaxes}) now and you're all set.</Text>
-                                            </div>
-                                        </Group>
-                                    </Paper>
-                                    <Paper p="lg" withBorder onClick={() => handleSelectPayment("cash-part")}>
-                                        <Group>
-                                            <Radio checked={payment === "cash-part"} />
-                                            <div>
-                                                <Title order={4}>Pay part now, part later</Title>
-                                                <Text>${checkoutDetails.totalBeforeTaxes / 2} due today, ${checkoutDetails.totalBeforeTaxes / 2} on {checkOutDate.add(3, 'days').format("MMM DD YYYY")}. No extra fees.</Text>
-                                            </div>
-                                        </Group>
-                                    </Paper>
-                                </Accordion.Panel>
-                            </Accordion.Item>
-
-                            <Accordion.Item value="credit-card">
-                                <Accordion.Control>
-                                    <Title order={4}>Credit card</Title>
-                                </Accordion.Control>
-                                <Accordion.Panel>
-                                    Please enter credit card details
-                                </Accordion.Panel>
-                            </Accordion.Item>
-                        </Accordion>
-                    </Paper>
-
-                </Grid.Col>
-                <Grid.Col span={5}>
-                    <Paper withBorder p={24} radius={10}>
-                        {campground &&
-                            <>
-                                <Group>
-                                    <Image height={130} width={200} radius="md" src={campground.images[0]?.url} alt="camp-image" />
-                                    <Stack>
-                                        <Title order={4}>
-                                            {campground.title}
-                                        </Title>
-                                        <Group align="baseline" spacing={5}>
-                                            <Text fz={18} span fw={500}>
-                                                ${campground.price.toFixed(0)}
-                                            </Text>
-                                            <Text span fz="md">
-                                                {' '}
-                                                / night
-                                            </Text>
-                                        </Group>
-                                        <Group spacing={5}>
-                                            {(campground.reviews && campground.reviews.length > 0) &&
-                                                <Group spacing={5} align="baseline">
-                                                    <IconStarFilled size={14} />
-                                                    <Text fw={700}>{campground.averageRating}</Text> · <Text opacity={0.5}>{campground.reviews.length} reviews</Text>
-                                                </Group>
-                                            }
-                                        </Group>
-                                    </Stack>
-                                </Group>
-                                <Divider my={20} />
-                                <Title order={2}>Price details</Title>
-                                <Group position="apart" fz="lg" my={20}>
-                                    <Text>{`$${campground?.price.toFixed(2)} x ${nights} ${nights > 1 ? "nights" : "night"}`}</Text>
-                                    <Text>${priceOfStay}</Text>
-                                </Group>
-                                {(nights >= 7) &&
-                                    <Group position="apart" fz="lg" my={20}>
-                                        <Text>Weekly stay discount</Text>
-                                        <Text c="#008A05"> -${weeklyStayDiscount} </Text>
+            {campground && 
+                <Grid gutter={55}>
+                    <Grid.Col span={7}>
+                        <Title mb="lg">
+                            Book your stay at {campground?.title}
+                        </Title>
+                        <Title order={3}>
+                            Dates
+                        </Title>
+                        <Text>
+                            {checkInDate.format("MMM DD")} - {checkOutDate.format("MMM DD")}
+                        </Text>
+                        <Divider my="lg" />
+                        <Title order={3} mb="lg">
+                            Choose how to pay
+                        </Title>
+                        <Paper withBorder>
+                            <Accordion
+                                mx="auto"
+                                variant="filled"
+                                defaultValue="cash"
+                                classNames={classes}
+                                className={classes.root}
+                            >
+                                <Accordion.Item value="cash">
+                                    <Accordion.Control>
+                                        <Title order={4}>Cash</Title>
+                                    </Accordion.Control>
+                                    <Accordion.Panel>
+                                        <Paper p="lg" withBorder mb="xs" onClick={() => handleSelectPayment("cash-full")}>
+                                            <Group>
+                                                <Radio checked={payment === "cash-full"} readOnly/>
+                                                <div>
+                                                    <Title order={4}>Pay in full</Title>
+                                                    <Text>Pay the total (${checkoutDetails.totalBeforeTaxes}) now and you're all set.</Text>
+                                                </div>
+                                            </Group>
+                                        </Paper>
+                                        <Paper p="lg" withBorder onClick={() => handleSelectPayment("cash-part")}>
+                                            <Group>
+                                                <Radio checked={payment === "cash-part"} readOnly/>
+                                                <div>
+                                                    <Title order={4}>Pay part now, part later</Title>
+                                                    <Text>${checkoutDetails.totalBeforeTaxes / 2} due today, ${checkoutDetails.totalBeforeTaxes / 2} on {checkOutDate.add(3, 'days').format("MMM DD YYYY")}. No extra fees.</Text>
+                                                </div>
+                                            </Group>
+                                        </Paper>
+                                    </Accordion.Panel>
+                                </Accordion.Item>
+                                <Accordion.Item value="credit-card">
+                                    <Accordion.Control>
+                                        <Title order={4}>Credit card</Title>
+                                    </Accordion.Control>
+                                    <Accordion.Panel>
+                                        <Text ml="xs">Please enter your credit card information below</Text>
+                                        <CreditCardForm />
+                                    </Accordion.Panel>
+                                </Accordion.Item>
+                            </Accordion>
+                        </Paper>
+                    </Grid.Col>
+                    <Grid.Col span={5}>
+                        <Paper withBorder p={24} radius={10}>
+                            <Group>
+                                <Image height={130} width={200} radius="md" src={campground.images[0]?.url} alt="camp-image" />
+                                <Stack>
+                                    <Title order={4}>
+                                        {campground.title}
+                                    </Title>
+                                    <Group align="baseline" spacing={5}>
+                                        <Text fz={18} span fw={500}>
+                                            ${campground.price.toFixed(0)}
+                                        </Text>
+                                        <Text span fz="md">
+                                            {' '}
+                                            / night
+                                        </Text>
                                     </Group>
-                                }
-                                <Group position="apart" fz="lg" my={20}>
-                                    <Group spacing={7}>
-                                        <HoverCard shadow="md" openDelay={200}>
-                                            <Text>YelpCamp service fee</Text>
-                                            <HoverCard.Target>
-                                                <IconInfoCircle size="1.2rem" style={{ opacity: 0.5 }} />
-                                            </HoverCard.Target>
-                                            <HoverCard.Dropdown>
-                                                <Text size="sm">This helps us run our platform and offer services like 24/7 support on your trip.</Text>
-                                            </HoverCard.Dropdown>
-                                        </HoverCard>
+                                    <Group spacing={5}>
+                                        {(campground.reviews && campground.reviews.length > 0) &&
+                                            <Group spacing={5} align="baseline">
+                                                <IconStarFilled size={14} />
+                                                <Text fw={700}>{campground.averageRating}</Text> · <Text opacity={0.5}>{campground.reviews.length} reviews</Text>
+                                            </Group>
+                                        }
                                     </Group>
-                                    <Text>${serviceFee}</Text>
+                                </Stack>
+                            </Group>
+                            <Divider my={20} />
+                            <Title order={2}>Price details</Title>
+                            <Group position="apart" fz="lg" my={20}>
+                                <Text>{`$${campground?.price.toFixed(2)} x ${nights} ${nights > 1 ? "nights" : "night"}`}</Text>
+                                <Text>${priceOfStay}</Text>
+                            </Group>
+                            {(nights >= 7) &&
+                                <Group position="apart" fz="lg" my={20}>
+                                    <Text>Weekly stay discount</Text>
+                                    <Text c="#008A05"> -${weeklyStayDiscount} </Text>
                                 </Group>
-                                <Divider my={20} />
-                                <Group position="apart" fz="lg" fw={700} my={20}>
-                                    <Text>Total before taxes</Text>
-                                    <Text>${totalBeforeTaxes}</Text>
+                            }
+                            <Group position="apart" fz="lg" my={20}>
+                                <Group spacing={7}>
+                                    <HoverCard shadow="md" openDelay={200}>
+                                        <Text>YelpCamp service fee</Text>
+                                        <HoverCard.Target>
+                                            <IconInfoCircle size="1.2rem" style={{ opacity: 0.5 }} />
+                                        </HoverCard.Target>
+                                        <HoverCard.Dropdown>
+                                            <Text size="sm">This helps us run our platform and offer services like 24/7 support on your trip.</Text>
+                                        </HoverCard.Dropdown>
+                                    </HoverCard>
                                 </Group>
-                                <Button radius={"md"} color="teal" fullWidth size="lg">
-                                    Reserve
-                                </Button>
-                            </>
-                        }
-                    </Paper>
-                </Grid.Col>
-            </Grid>
+                                <Text>${serviceFee}</Text>
+                            </Group>
+                            <Divider my={20} />
+                            <Group position="apart" fz="lg" fw={700} my={20}>
+                                <Text>Total before taxes</Text>
+                                <Text>${totalBeforeTaxes}</Text>
+                            </Group>
+                            <Button radius={"md"} color="teal" fullWidth size="lg">
+                                Reserve
+                            </Button>
+                        </Paper>
+                    </Grid.Col>
+                </Grid>
+            }
         </Container>
     )
 }
