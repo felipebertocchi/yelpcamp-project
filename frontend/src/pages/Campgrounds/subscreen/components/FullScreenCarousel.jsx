e494d66d8f27678297d53809e378fc2942f8e284
felipebertocchi <fabertocchi@gmail.com>
2024-01-18 22:23:36 -0300

    fix(FullScreenCarousel): fix incorrect slide offset

--- frontend/src/pages/Campgrounds/subscreen/components/FullScreenCarousel.jsx
+++ frontend/src/pages/Campgrounds/subscreen/components/FullScreenCarousel.jsx
@@ -1,12 +1,21 @@
-import { Carousel } from '@mantine/carousel';
+import { useState } from 'react';
+import { Carousel, useAnimationOffsetEffect } from '@mantine/carousel';
 import { CloseButton, Group, Image, Modal, Overlay, rem } from "@mantine/core";
 import { IconChevronLeft, IconChevronRight } from '@tabler/icons-react';
 
 export default function ({ images, open, handleOpen, initialSlide }) {
+    const TRANSITION_DURATION = 200;
+    const [embla, setEmbla] = useState(null);
+
+    useAnimationOffsetEffect(embla, TRANSITION_DURATION);
+
     return (
         <Modal
             opened={open}
             onClose={() => handleOpen.close()}
+            transitionProps={{
+                duration: TRANSITION_DURATION
+            }}
             fullScreen
             withCloseButton={false}
         >
@@ -15,17 +24,20 @@ export default function ({ images, open, handleOpen, initialSlide }) {
                     <CloseButton title="Close" size="xl" radius="md" iconSize={20} onClick={() => handleOpen.close()} aria-label="Close" />
                 </Group>
                 <Carousel
+                    getEmblaApi={setEmbla}
                     initialSlide={initialSlide}
                     draggable={false}
                     withIndicators
                     loop
                     controlSize={40}
+                    height="100%"
+                    style={{ flex: 1 }}
                     nextControlIcon={<IconChevronRight size={20} />}
                     previousControlIcon={<IconChevronLeft size={20} />}
                 >
                     {images?.map(({ url }, index) =>
                         <Carousel.Slide key={index} p={80}>
-                            <Image src={url} fit={'contain'} height={'80vh'} withPlaceholder />
+                            <Image src={url} fit='contain' height="80vh" withPlaceholder />
                         </Carousel.Slide>
                     )}
                 </Carousel>
